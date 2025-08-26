94617d8707f3e412bf12a64f5009613f545e790f
hariharanp05 <128158383+hariharanp05@users.noreply.github.com>
2025-08-26 23:27:33 +0530

    Delete backend directory

--- backend/portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('save/', views.save_portfolio, name='save-portfolio'),
-    path('user/', views.get_portfolio_by_user, name='get-user-portfolio'),
-    path('<str:user_id>/delete/', views.delete_portfolio, name='delete-portfolio'),
-    path('public/<str:username>/', views.get_public_portfolio, name='get_public_portfolio'),
-]
